687aed739a8e4fbfafcd7a7b0f60afe00fb5eba9
Guido Solar <guido.solar.c@gmail.com>
2022-06-08 09:58:57 -0400

    Configuracion inicial y conexion a API Symfony

--- src/App.js
+++ src/App.js
@@ -1,22 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Form, Row, Col, InputGroup, FormControl, Button, Alert } from 'react-bootstrap';
+import logo from './buk-logo.svg';
 import './App.css';
 
 function App() {
+  // Variable para enviar a API
+  const [inputValue, setInputValue] = useState()
+
+  // Variables (estados), para contener respuesta de API
+  const [msgAPI, setMSG] = useState()
+  const [valorAPI, setValorAPI] = useState()
+  const [primosAPI, setPrimosAPI] = useState()
+  const [statusAPI, setStatus] = useState()
+  // Comunicacion con API local
+  const url = 'http://127.0.0.1:8000/'
+  // Funcion para obtener API
+  const obtenerAPI = async () => {
+    const respuesta = await fetch(url + 'prueba/numerosPrimos', {     
+        method: "POST",
+        body: JSON.stringify({
+          valor: inputValue
+        })
+    })  
+    const plainRespuesta = await respuesta
+    const responseJSON = await respuesta.json()
+    // Guardo las respuestas
+    setStatus(plainRespuesta.status)
+    setValorAPI(plainRespuesta.valorRecibido)
+    // setValorAPI(responseJSON.valorRecibido)
+    setPrimosAPI(responseJSON.numerosPrimos)
+    setMSG(responseJSON.msg)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Row>
+            <Col md="6">
+              <Form.Label htmlFor="basic-url">Números primos</Form.Label>
+              <InputGroup> 
+                <FormControl
+                  placeholder="Ej: 6"
+                  aria-label="Parámetro"
+                  aria-describedby="Input para parámetro"
+                  onChange={(e) => setInputValue(e.target.value)}
+                />
+                <Button variant="primary" id="input-parametro" onClick={ obtenerAPI }>
+                  Aceptar
+                </Button>
+              </InputGroup>
+            </Col>
+          </Row>
+          <Row className='mt-3'>
+            { statusAPI === 200 &&
+              <Col md="4">
+                <Alert variant="success">
+                  <p>Parámetro recibido: { valorAPI }</p>
+                  <p>Respuesta: { primosAPI }</p>
+                  <p>Mensaje: { msgAPI }</p>
+                </Alert>
+              </Col> 
+            } 
+          </Row>
+        </Container>       
       </header>
     </div>
   );
